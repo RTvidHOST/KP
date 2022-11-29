c67308c5a06624c7a39aa88e25e436ef6ede8d18
RTvidHOST <77573582+RTvidHOST@users.noreply.github.com>
2022-11-29 17:31:23 +0300

    Update Configs.java

--- src/sample/Configs.java
+++ src/sample/Configs.java
@@ -5,6 +5,6 @@ public class Configs {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "nickreskoP333";
+    protected String dbPass = "";
     protected String dbName = "kp";
 }
